add1d4703a0b87cb4042b757b29df3951e2d9cef
Timothy Gonzalez <timg456789@yahoo.com>
2025-12-23 02:37:39 -0500

    try affiliate proof again.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
 import { Navigation } from "@/components/Navigation";
@@ -27,13 +28,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name='impact-site-verification' content='06c0e7fa-c882-4a1c-8573-edca953f919e'/>
-        <script type="text/javascript" src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=e7db51ace1f37e7d2389bba2c897aa39e3be5c92"></script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={inter.className}>
+        {/* AvantLink Affiliate Verification */}
+        <Script
+          src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=e7db51ace1f37e7d2389bba2c897aa39e3be5c92"
+          strategy="beforeInteractive"
+        />
         <Navigation />
         {children}
         <Footer />
